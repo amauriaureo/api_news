e861ac7d1b733f74d59eda06a4cfd278d35a06d6
Amauri Rodrigues <amauriaureo@gmail.com>
2023-01-01 23:17:48 -0300

    Requisicao e resposta em API com Express

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,23 @@
-const soma = (req, res) => {
-    const soma = 100 + 1
+const create = (req, res) => {
+    const {name, username, email, password, avatar, background} = req.body;
 
-  res.send({soma: soma})
+    if (!name || !username || !email || !password || !avatar || !background) {
+        res.status(400).send({message: "Submit all fields for registration."})
+    }
+
+    // console.log(user);
+
+    res.status(201).send({
+        message: "User created succesfully",
+        user: {
+            name,
+            username,
+            email,
+            // password,
+            avatar,
+            background
+        },
+    });
 };
 
-module.exports = {soma};
+module.exports = { create };
