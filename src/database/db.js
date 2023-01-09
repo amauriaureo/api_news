5571a62a13f28ad1316d1ba2d1ad253f7c2f8f92
Amauri Rodrigues <amauriaureo@gmail.com>
2023-01-08 22:14:41 -0300

    dotenv em express

--- src/database/db.js
+++ src/database/db.js
@@ -4,8 +4,7 @@ const connectDatabase = () => {
     console.log("Wait connecting to the database...")
 
     mongoose
-        .connect(
-            "mongodb+srv://root:YLBO0jooU2W46BpJ@cluster0.u98jwyp.mongodb.net/?retryWrites=true&w=majority",
+        .connect( process.env.MONGODB_URI,
             { useNewUrlParser: true, useUnifiedTopology: true }
         )
         .then(() => console.log("MongoDB Atlas Connected."))
