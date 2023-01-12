55e51487d04a2348c53b5b8e1a4cd927fbf63dba
Amauri Rodrigues <amauriaureo@gmail.com>
2023-01-12 08:57:52 -0300

    login e senha com bcrypt no express

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,21 @@
 import { loginService } from "../services/auth.service.js";
-// import bcrypt from bcrypt;
+import bcrypt from 'bcrypt';
 
 const login = async (req, res) => {
     const { email, password } = req.body;
 try {
     const user = await loginService(email);
+    if(!user) {
+        return res.status(404).send ({message: "User or password not found."})
+    }
 
-    // const passwordIsValid = bcrypt.compare(password, user.password)
+    const passwordIsValid = bcrypt.compareSync(password, user.password)
+    
+    if (!passwordIsValid){
+        return res.status(404).send ({message: "User or password not found."})
+    }
 
-    res.send(user);
+    res.send("Login ok.");
 
 } catch (err){
     res.status(500).send(err.message)
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js'
 
-const loginService = (email) => User.findOne({email: email});
+const loginService = (email) => 
+    User.findOne({email: email}).select("+password");
 
 
 export {loginService}
