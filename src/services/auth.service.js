ffd28fe69a61c8fec73a4b2983cec5d976d1869e
Amauri Rodrigues <amauriaureo@gmail.com>
2023-01-12 21:03:53 -0300

    iniciando jwt na api em express

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,10 @@
 import User from '../models/User.js'
+import jwt from 'jsonwebtoken';
 
 const loginService = (email) => 
     User.findOne({email: email}).select("+password");
+
+const generateToken = () => jwt.sign({id: id}, "qualquercoisa");
 
 
 export {loginService}
