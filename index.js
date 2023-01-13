0c1b9b3ff4ff1bb0330fcf23aa2732228e0eae62
Amauri Rodrigues <amauriaureo@gmail.com>
2023-01-13 08:42:34 -0300

    feat: add create News

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv"
 
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
+import newsRoute from "./src/routes/news.route.js";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -15,5 +16,6 @@ connectDatabase()
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
+app.use("/news", newsRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
---
+++ src/services/news.service.js
@@ -0,0 +1,7 @@
+import News from '../models/News.js';
+
+const createService = (body) => News.create(body);
+
+const findAllService = () => News.find();
+
+export default { createService};
