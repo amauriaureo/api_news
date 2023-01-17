1a71e16b3420813343e4821130f84b97865a5d01
Amauri Rodrigues <amauriaureo@gmail.com>
2023-01-16 22:51:00 -0300

    proteger uma rota com autenticação JWT

--- src/controllers/news.controllers.js
+++ src/controllers/news.controllers.js
@@ -3,6 +3,24 @@ import { createService, findAllService } from "../services/news.service.js";
 
 const create = async (req, res) => {
     try {
+        const { authorization } = req.headers;
+        
+        if (!authorization) {
+            return res.send(401);
+        }
+
+        const parts = authorization.split(" ")
+
+        const [schema, token] = parts
+
+        if (parts.length !== 2) {
+            return res.send(401);
+        }
+
+        if (schema !== "Bearer") {
+            return res.send(401);
+        }
+
         const {title, text, banner} = req.body;
         if (!title || !banner || !text) {
             res.status(400).send({
