b09e34099d6c69d620f596d7a67bd91e442e3f2f
Amauri Rodrigues <amauriaureo@gmail.com>
2023-01-19 07:22:44 -0300

    busca pelo titulo no MongoDB utilizando expressao regular com express

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 const router = Router();
 
-import {create, findAll, topNews, findById} from "../controllers/news.controllers.js";
+import {
+    create, 
+    findAll, 
+    topNews, 
+    findById, 
+    searchByTitle
+} from "../controllers/news.controllers.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 router.post("/", authMiddleware, create);
 router.get("/", findAll);
 router.get("/top", topNews);
-router.get("/:id", findById);
+router.get("/search", searchByTitle);
+
+
+router.get("/:id", authMiddleware, findById);
 
 export default router;
